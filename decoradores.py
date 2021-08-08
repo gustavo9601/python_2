8bf4c008a813d57fb34ebaecdfb9ccb641d0dd4c
gustavo9601 <tavo9601@gmail.com>
2021-08-08 18:36:51 -0500

    add decoradores

--- decoradores.py
+++ decoradores.py
@@ -1,17 +1,66 @@
+from datetime import datetime
 """
-Decoradores:
-Son wrapper que enpaquetan una funcion, y que a su ves reciben esa funcion como parametro
+Decoradores: (Un clousere espcial)
+Son wrapper que empaquetan una funcion, y que a su ves reciben esa funcion como parametro
 Permite ejecutar al inicio y al fin cierta logica, como exteneder la funcionalidad
 """
 
 
-def hola():
-    def bienvenido():
-        return "Bienvenido"
-    return bienvenido
+def decorador(func):
+    # print(type(func))
+    def envoltura():
+        print('Añade al inicio')
+        func()
+        print('Añade al final')
 
-print("hola()", hola())
+    return envoltura
 
+@decorador
+def saludo():
+    print('Holaaa!')
+
+saludo()
+print("*" * 30)
+
+
+def mayusculas(func):
+    def envoltura(text):
+        return func(text).upper()
+    return envoltura
+
+@mayusculas
+def mensaje(nombre):
+    return f"{nombre}, ahora en Mayus"
+
+print(mensaje('Gustavo'))
+print("*" * 30)
+
+
+def execution_time(func):
+    # Envoltura
+    # *args, **kwargs // se pueden enviar o no argumentos posionales o de llave
+    def wrapper(*args, **kwargs):
+        initial_time = datetime.now()
+        func(*args, **kwargs)
+        final_time = datetime.now()
+        # .total_seconds()  // func of datetime to get the seconds from a diff dates
+        seconds_elapsed = (final_time - initial_time).total_seconds()
+        print(f"Pasaron {seconds_elapsed} segundos.")
+    return wrapper
+
+@execution_time
+def random_func():
+    for _ in range(1,10000000):
+        pass
+
+@execution_time
+def random_sum(a, b):
+    return a + b
+
+random_func()
+random_sum(1,2)
+
+print("*" * 30)
 
 def smart_division(div_funcion):
     def div(a, b):
@@ -21,6 +70,7 @@ def smart_division(div_funcion):
         return div_funcion(a, b)
 
     return div
+
 
 # @name of function decorador
 @smart_division
@@ -55,5 +105,6 @@ print(f"suma(10,100) : {suma(10, 100)}")
 def division_2(a, b):
     return a / b
 
-print("**************************************")
+
+print("*" * 30)
 print(f"division_2(50,2): {division_2(50, 0)}")
---
+++ scope.py
@@ -0,0 +1,8 @@
+
+# Variable de Global escope
+y = 5
+
+# Alcance local
+def local_scope():
+    x = 5
+    print(f"x: {x}")
